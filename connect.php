9a61acc7f9ff78ffe87edfba7a6789f57d03d3a5
Víctor Olea <90351946+VictorOlea@users.noreply.github.com>
2022-09-21 20:33:52 -0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+$Temperatura = $_GET['temp'];
+$Humedad = $_GET['hum'];
+$HumedadSuelo = $_GET['humSoil'];
+
+echo "La Temperatura es: ".$Temperatura."<br>La Humedad es: ".$Humedad."<br>La Humedad del Suelo es: ".$HumedadSuelo;
+
+$usuario = "root";
+$contrasena = "";
+$servidor = "localhost";
+$basededatos = "test";
+
+$conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de dato");
+
+$db = mysqli_select_db( $conexion, $basededatos) or die ("No se ha podido seleccionar la base de datos");
+
+//$Creado_en = time();
+
+$consulta = "INSERT INTO iot_proyecto (Temperatura, Humedad, HumedadSuelo) VALUES ( ".$Temperatura.", ".$Humedad.", ".$HumedadSuelo.")";
+
+$resultado = mysqli_query( $conexion, $consulta);
+
+?>
